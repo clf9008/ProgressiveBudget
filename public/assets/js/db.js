0d4f293b75474022771102e5ec42a9546de41eb8
Follett <clf9008@gmail.com>
2021-02-21 17:18:25 -0700

    updated db.js with call to server to store updated transactions when back online

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -34,3 +34,31 @@ function saveRecord(record) {
   store.add(record);
 }
 
+//Function called when user goes online to send transactions stored in db to server
+function checkDatabase() {
+  const transaction = db.transaction("pending", "readonly");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction("pending", "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+//Event listener listening for when the app comes back online
+window.addEventListener("online", che
