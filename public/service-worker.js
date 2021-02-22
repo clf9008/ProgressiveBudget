21cef94c92f36b07a2ae90ae50129018dd4ae82f
Follett <clf9008@gmail.com>
2021-02-21 17:07:40 -0700

    updated errors in service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,66 +1,75 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "assets/css/styles.css",
-    "/assets/js/index.js",
-    "/assets/js/db.js",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+ "/",
+  "/index.html",
+  "/assets/css/styles.css",
+  "/assets/js/index.js",
+  "/assets/js/db.js",
+  "/assets/images/icons/icon-192x192.png",
+  "/assets/images/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
-//create or load caches
+//install cache upon app startup
 self.addEventListener("install", (evt) => {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
-});
-//remove old caches
-self.addEventListener("activate", (evt) => {
-    evt.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(
-                keyList.map((key) => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-        })
-    );
-    self.clients.claim();
+  self.skipWaiting();
 });
-//cache successfull GET request to API
+
+self.addEventListener("activate", (evt) => {
+  //Remove old caches 
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (evt) => {
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET" {
-        evt.respondWith(
-            caches
-                .open(DATA_CACHE_NAME)
-                .then((cache) => {
-                    return fetch(evt.request)
-                        .then((response) => {
-                            if (response.status === 200) {
-                                cache.put(evt.request, response.clone());
-                            }
-                            
-                            return response;
-                        })
-                        .catch((err) => console.log(err))
-                );
-                
-        return;
+  //If cache successful GET requests to API
+  if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              //If response good, clone it and store it in cache
+              if (response.status === 200) {
+                cache.put(evt.request, response.clone());
+              }
+
+              return response;
+            })
+            .catch(() => {
+              //If the request failed, try to get it from the cache
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
     }
     
-    evt.respondWith(
-        caches.match(evt.request).then((response) => {
-            return response || fetch(evt.request);
-        })
-    );
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
 });
