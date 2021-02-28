61b2b02b8eefa5a368e52699f9245192e12185c3
Follett <clf9008@gmail.com>
2021-02-28 14:03:26 -0700

    updagted server.js to connect to MondoAtlasDB for deployment

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/budget',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   );
 
 app.use(require("./routes/api.js"));
 
